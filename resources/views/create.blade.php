dc68d20354605e78b5fc7c0a238fb3d0d151e0bd
tk <83660733+tk-21@users.noreply.github.com>
2021-12-23 21:47:17 +0900

    メモに既存タグを付けられるように改良

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -8,6 +8,14 @@
         <div class="form-group">
             <textarea class="form-control mb-3" name="content" rows="3" placeholder="ここにメモを入力"></textarea>
         </div>
+        @foreach($tags as $tag)
+        <div class="form-check form-check-inline mb-3">
+            <!-- name属性で複数の値を送りたいときは配列形式にしておく -->
+            <input class="form-check-input" type="checkbox" name="tags[]" id="{{ $tag['id'] }}" value="{{ $tag['id'] }}">
+            <label class="form-check-label" for="{{ $tag['id'] }}">{{ $tag['name'] }}</label>
+        </div>
+        @endforeach
+
         <input type="text" class="form-control w-50 mb-3" name="new_tag" placeholder="新しいタグ">
         <button type="submit" class="btn btn-primary">保存</button>
     </form>
