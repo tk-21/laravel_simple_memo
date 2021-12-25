fe6da40d1a013a5c9fd7f2068e3aace11cc5adf7
tk <83660733+tk-21@users.noreply.github.com>
2021-12-25 15:09:39 +0900

    タグからの絞り込み検索実装

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Models\Memo;
+use App\Models\Tag;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,14 +27,31 @@ class AppServiceProvider extends ServiceProvider
     {
         //全てのメソッドが呼ばれる前に先に呼ばれるメソッド
         view()->composer('*', function ($view) {
-            //必要な条件で絞ってメモを取得
-            $memos = Memo::select('memos.*')
+            $query_tag = \Request::query('tag');//Requestファザードを使う。バックスラッシュをつけるとインポートしていなくても使えるようになる
+
+            if(!empty($query_tag)) {
+                $memos = Memo::select('memos.*')
+                ->leftJoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')//結合してから絞り込む
+                ->where('memo_tags.tag_id', '=', $query_tag)
                 ->where('user_id', '=', \Auth::id()) //ログインしているユーザーによって動的に変わるようにする
                 ->whereNull('deleted_at') //deleted_atに値があるものは削除されたとみなす
                 ->orderBy('updated_at', 'DESC')
                 ->get();
+            } else {
+                //必要な条件で絞ってメモを取得
+                $memos = Memo::select('memos.*')
+                    ->where('user_id', '=', \Auth::id()) //ログインしているユーザーによって動的に変わるようにする
+                    ->whereNull('deleted_at') //deleted_atに値があるものは削除されたとみなす
+                    ->orderBy('updated_at', 'DESC')
+                    ->get();
+            }
 
-                $view->with('memos', $memos);
+            $tags = Tag::where('user_id', '=', \Auth::id())
+                ->whereNull('deleted_at')
+                ->orderBy('id', 'DESC')
+                ->get();
+
+            $view->with('memos', $memos)->with('tags', $tags);
         });
     }
 }
